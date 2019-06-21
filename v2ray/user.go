8eeeced410403983dba53243f0737d107fa523dc
shynome <shynome@gmail.com>
2019-06-21 16:45:17 +0800

    user add rm ok

---
+++ v2ray/user.go
@@ -0,0 +1,101 @@
+package v2ray
+
+import (
+	"context"
+
+	"sync"
+
+	"google.golang.org/grpc"
+	"v2ray.com/core/app/proxyman/command"
+	"v2ray.com/core/common/protocol"
+	"v2ray.com/core/common/serial"
+	"v2ray.com/core/proxy/vmess"
+)
+
+func (v2 V2ray) getClient() (grpcClient command.HandlerServiceClient, err error) {
+	if v2.GrpcClient != nil {
+		return v2.GrpcClient, nil
+	}
+	cc, err := grpc.Dial(v2.GrpcAddr, grpc.WithInsecure())
+	if err != nil {
+		return
+	}
+	v2.GrpcClient = command.NewHandlerServiceClient(cc)
+	grpcClient = v2.GrpcClient
+	return
+}
+
+func (v2 V2ray) addUser(user User) (err error) {
+	hsClient, err := v2.getClient()
+	if err != nil {
+		return
+	}
+	v2User := &protocol.User{
+		Email: user.Email,
+		Account: serial.ToTypedMessage(&vmess.Account{
+			Id:      user.UUID,
+			AlterId: 64,
+		}),
+	}
+	_, err = hsClient.AlterInbound(context.Background(), &command.AlterInboundRequest{
+		Tag:       v2.Tag,
+		Operation: serial.ToTypedMessage(&command.AddUserOperation{User: v2User}),
+	})
+	if err != nil {
+		return
+	}
+	return
+}
+
+var mux = sync.Mutex{}
+
+func (v2 V2ray) loopUsers(fn func(user User) error) func(users []User) []error {
+	return func(users []User) (errs []error) {
+		mux.Lock()
+		defer mux.Unlock()
+		wait := make(chan int)
+		errs = []error{}
+		var finshedTaskCount = 0
+		var allTaskCount = len(users)
+		for _, user := range users {
+			wrapper := func() {
+				err := fn(user)
+				finshedTaskCount++
+				if err != nil {
+					errs = append(errs, err)
+				}
+				if allTaskCount == finshedTaskCount {
+					wait <- 1
+				}
+			}
+			go wrapper()
+		}
+		<-wait
+		return
+	}
+}
+
+// AddUsers v2ray
+func (v2 V2ray) AddUsers(users []User) (errs []error) {
+	return v2.loopUsers(v2.addUser)(users)
+}
+
+func (v2 V2ray) removeUsers(user User) (err error) {
+	hsClient, err := v2.getClient()
+	if err != nil {
+		return
+	}
+	_, err = hsClient.AlterInbound(context.Background(), &command.AlterInboundRequest{
+		Tag:       v2.Tag,
+		Operation: serial.ToTypedMessage(&command.RemoveUserOperation{Email: user.Email}),
+	})
+	if err != nil {
+		return
+	}
+	return
+}
+
+// RemoveUsers v2ray
+func (v2 V2ray) RemoveUsers(users []User) (errs []error) {
+	return v2.loopUsers(v2.removeUsers)(users)
+}
--- v2ray/v2ray.go
+++ v2ray/v2ray.go
@@ -1,7 +1,24 @@
 package v2ray
 
+import (
+	"github.com/shynome/v2ldap/ldap"
+	"v2ray.com/core/app/proxyman/command"
+)
+
+// V2ray remote handler wrapper
+type V2ray struct {
+	Tag        string
+	GrpcAddr   string
+	GrpcClient command.HandlerServiceClient
+	Ldap       ldap.LDAP
+}
+
+// User v2ray
+type User struct {
+	Email string
+	UUID  string
+}
+
 func init() {
-	initLdapInstance()
-	initMemoryUsers()
 	initV2rayConfig()
 }
