f3f1185bf333e787e8f239a2c39846c85a78b3ff
shynome <shynome@gmail.com>
2019-06-24 11:40:39 +0800

    init from env

--- server/common/v2ray.go
+++ server/common/v2ray.go
@@ -1,6 +1,10 @@
 package common
 
 import (
+	"os"
+
+	"strconv"
+
 	"github.com/shynome/v2ldap/v2ray"
 )
 
@@ -8,11 +12,30 @@ import (
 var V2ray *v2ray.V2ray
 
 func initV2ray() {
-	if V2ray.DB != nil {
+	if V2ray != nil {
 		return
 	}
+	remoteTag, remoteGrpc, apiPortEnv := os.Getenv("RemoteTag"), os.Getenv("RemoteGrpc"), os.Getenv("V2rayAPIPort")
+	var apiPort uint32
+	if remoteTag == "" {
+		remoteTag = "ws"
+	}
+	if remoteGrpc == "" {
+		remoteGrpc = "127.0.0.1:3001"
+	}
+	if apiPortEnv == "" {
+		apiPortEnv = "3001"
+	}
+	if port, err := strconv.Atoi(apiPortEnv); err != nil {
+		panic(err)
+	} else {
+		apiPort = uint32(port)
+	}
 	V2ray = &v2ray.V2ray{
-		DB: GetDB(),
+		DB:         GetDB(),
+		APIPort:    apiPort,
+		RemoteTag:  remoteTag,
+		RemoteGrpc: remoteGrpc,
 	}
 	users, err := Ldap.GetUsers()
 	if err != nil {
