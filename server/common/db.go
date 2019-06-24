38d47fe9723ba37ec3c6e95ab669e9cde47b6c90
shynome <shynome@gmail.com>
2019-06-24 15:02:41 +0800

    DB_PATH 可指定

--- server/common/db.go
+++ server/common/db.go
@@ -1,6 +1,8 @@
 package common
 
 import (
+	"os"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite" // init sqlite
 	"github.com/shynome/v2ldap/v2ray"
@@ -13,7 +15,11 @@ var DB *gorm.DB
 func GetDB() *gorm.DB {
 	var err error
 	if DB == nil {
-		if DB, err = gorm.Open("sqlite3", "test.db"); err != nil {
+		dbpath := os.Getenv("DB_PATH")
+		if dbpath == "" {
+			dbpath = "v2ldap.db"
+		}
+		if DB, err = gorm.Open("sqlite3", dbpath); err != nil {
 			panic(err)
 		}
 		if err = DB.AutoMigrate(&v2ray.User{}).Error; err != nil {
