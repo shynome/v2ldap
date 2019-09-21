98c249d81d74a7ace5f5b10ec5ed6108c17e3f18
shynome <shynome@gmail.com>
2019-09-22 07:07:07 +0800

    no LDAP env , no export /ldap api

--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"net/http"
 
+	"github.com/shynome/v2ldap/server/common"
 	"github.com/shynome/v2ldap/server/ldap"
 	"github.com/shynome/v2ldap/server/v2ray"
 )
@@ -11,6 +12,8 @@ import (
 var APIMux = http.NewServeMux()
 
 func init() {
-	APIMux.Handle("/ldap/", ldap.APIMux)
+	if common.Ldap.BindDN != "" {
+		APIMux.Handle("/ldap/", ldap.APIMux)
+	}
 	APIMux.Handle("/v2ray/", v2ray.APIMux)
 }
