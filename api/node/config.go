003c4b2fefe3638b719ebda45f5bbfe8afd1fa33
shynome <shynome@gmail.com>
2020-05-05 21:01:03 +0800

    支持环境变量配置

--- api/node/config.go
+++ api/node/config.go
@@ -12,10 +12,12 @@ import (
 
 func configHandler(c echo.Context) (err error) {
 	db := model.GetDB(c)
-	v2 := &v2ray.V2ray{}
+	v2 := v2ray.New()
 	var users = []model.User{}
-	if err = db.Unscoped().Where("version = ?", gorm.Expr("NULL")).Find(&users).Error; err != nil {
-		return c.String(500, "查找用户出错")
+	if false {
+		if err = db.Where("version = ?", gorm.Expr("NULL")).Find(&users).Error; err != nil {
+			return c.String(500, "查找用户出错")
+		}
 	}
 	config := v2.GenConfig(users)
 	pbconfig, err := proto.Marshal(config)
--- v2ray/v2ray.go
+++ v2ray/v2ray.go
@@ -1,9 +1,36 @@
 package v2ray
 
+import (
+	"os"
+	"strconv"
+)
+
 // V2ray V2ray Config
 type V2ray struct {
 	WsPort    uint32 // ws port
 	WsPath    string // ws path
 	VNEXT     string // 如果有 VNEXT 将会替换默认 outbound
 	SocksPort uint32 // 如果有值则暴露一个无需认证的 socks 端口
+}
+
+// New V2ray
+func New() *V2ray {
+	v2 := &V2ray{}
+	if wsPath := os.Getenv("WsPath"); wsPath != "" {
+		v2.WsPath = wsPath
+	}
+	if wsPort := os.Getenv("WsPort"); wsPort != "" {
+		if port, err := strconv.ParseUint(wsPort, 10, 32); err == nil {
+			v2.WsPort = uint32(port)
+		}
+	}
+	if VNEXT := os.Getenv("VNEXT"); VNEXT != "" {
+		v2.VNEXT = VNEXT
+	}
+	if socksPort := os.Getenv("VNEXTSocksPort"); socksPort != "" {
+		if port, err := strconv.ParseUint(socksPort, 10, 32); err == nil {
+			v2.SocksPort = uint32(port)
+		}
+	}
+	return v2
 }
